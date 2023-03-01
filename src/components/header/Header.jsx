fb3233cbd570d96eeb7d39eea27e0c291b047bf6
tahasafdari <tahahasan.safdari@gmail.com>
2023-03-01 17:34:17 +0200

    lottie icon added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import "./header.css";
 import Download from "./Download";
 import HeaderSocials from "./HeaderSocials";
+import webDesign from "./webDesign.json";
+import Lottie from "lottie-react";
+
 const Header = () => {
   return (
     <div className="header_cont">
       <div className="container header__container">
-        <h4>Hi, my name is</h4>
-        <h1 className="name">Hasan Safdari.</h1>
-        <h2>I am a software developer student.</h2>
-        <Download />
-        <HeaderSocials />
+        <div className="flex">
+          <div className="left">
+            <h4>Hi, my name is</h4>
+            <h1 className="name">Hasan Safdari.</h1>
+            <h2>I am a software developer student.</h2>
+            <div className="flex-container"></div>
+            <Download />
+            <HeaderSocials />
+          </div>
+          <div className="right">
+            <div className="lottieIcon">
+              <Lottie animationData={webDesign} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,13 +1,38 @@
-import React from "react";
 import "./nav.css";
 import { FiHome } from "react-icons/fi";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBookAlt } from "react-icons/bi";
+import { Link } from "react-router-dom";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Nav = () => {
   const [activeNav, setActiveNav] = useState("#");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const aboutUs = document.querySelector("#about");
+      const homePage = document.querySelector("#");
+      const projects = document.querySelector("#experience");
+      const aboutUsTop = aboutUs.offsetTop;
+      const homePageTop = homePage.offsetTop;
+      const projectsTop = projects.offsetTop;
+
+      if (window.scrollY >= homePageTop && window.scrollY < aboutUsTop) {
+        setActiveNav("#");
+      } else if (window.scrollY >= aboutUsTop && window.scrollY < projectsTop) {
+        setActiveNav("about");
+      } else if (window.scrollY >= projectsTop) {
+        setActiveNav("experience");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <nav className="nav">
       <a
@@ -31,6 +56,29 @@ const Nav = () => {
       >
         <BiBookAlt />
       </a>
+
+      {/*
+      <a
+        href="#top"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <FiHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBookAlt />
+      </a>*/}
     </nav>
   );
 };
